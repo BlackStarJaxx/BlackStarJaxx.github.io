c02f2557152df29282a041f98fad77be756e489b
Bohdan <45693914+BlackStarJaxx@users.noreply.github.com>
2020-06-22 23:52:22 +0300

    try

--- assets/js/main.js
+++ assets/js/main.js
@@ -196,7 +196,7 @@
         var formID = $(this).attr('id');
         var formNm = $('#' + formID);
         var message = $(formNm).find(".form-message");
-        // var formTitle = $(formNm).find(".form-title");
+        var formTitle = $(formNm).find(".form-title");
         $.ajax({
             type: "POST",
             url: './forms/send-message-to-telegram.php',
@@ -204,7 +204,7 @@
             success: function (data) {
               // Вывод сообщения об успешной отправке
               message.html(data);
-              // formTitle.css("display","none");
+              formTitle.css("display","none");
               setTimeout(function(){
                 formTitle.css("display","block");
                 message.html('');
@@ -214,9 +214,9 @@
             error: function (jqXHR, text, error) {
                 // Вывод сообщения об ошибке отправки
                 message.html(error);
-                // formTitle.css("display","none");
+                formTitle.css("display","none");
                 setTimeout(function(){
-                  // formTitle.css("display","block");
+                  formTitle.css("display","block");
                   message.html('');
                   $('input').not(':input[type=submit], :input[type=hidden]').val('');
                 }, 3000);
