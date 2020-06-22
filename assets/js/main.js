4e28eaea366b2e60a6491c645bdb8285c26feb28
Bohdan <45693914+BlackStarJaxx@users.noreply.github.com>
2020-06-23 00:39:26 +0300

    t2

--- assets/js/main.js
+++ assets/js/main.js
@@ -190,40 +190,4 @@
 
 
 
-  //telegram bot request
-  $(document).ready(function () {
-    $(".php-email-form").submit(function () {
-        var formID = $(this).attr('id');
-        var formNm = $('#' + formID);
-        var message = $(formNm).find(".form-message");
-        var formTitle = $(formNm).find(".form-title");
-        $.ajax({
-            type: "POST",
-            url: './forms/send-message-to-telegram.php',
-            data: formNm.serialize(),
-            success: function (data) {
-              // Вывод сообщения об успешной отправке
-              message.html(data);
-              formTitle.css("display","none");
-              setTimeout(function(){
-                formTitle.css("display","block");
-                message.html('');
-                $('input').not(':input[type=submit], :input[type=hidden]').val('');
-              }, 3000);
-            },
-            error: function (jqXHR, text, error) {
-                // Вывод сообщения об ошибке отправки
-                message.html(error);
-                formTitle.css("display","none");
-                setTimeout(function(){
-                  formTitle.css("display","block");
-                  message.html('');
-                  $('input').not(':input[type=submit], :input[type=hidden]').val('');
-                }, 3000);
-            }
-        });
-        return false;
-    });
-});
-
 })(jQuery);
